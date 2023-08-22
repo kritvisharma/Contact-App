db2fdc5ec8d79e026172fe25c150f264b4209679
Kritvi Sharma <kritvisharma0912@gmail.com>
2023-08-23 01:23:14 +0530

    First Commit

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+import Addcontact from './Addcontact';
+import Contactlist from './Contactlist';
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Addcontact />
+      <Contactlist /> 
+    </div>
+  );
+}
+
+export default App;
