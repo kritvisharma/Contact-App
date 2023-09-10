028676dd2f6cf32ca379599b2dc4be1e427d96bd
Kritvi Sharma <kritvisharma0912@gmail.com>
2023-09-10 23:02:38 +0530

    Commit 2

---
+++ src/components/contactcard.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const CardContact =(props)=>{
+    return (
+        <div className="item ">
+                <div className="content">
+                    <div className="header">{contact.name}</div>
+                    <div>{contact.email}</div>
+                    <div>
+                    <i className="trash alternate outline icon">
+                        style={{color:"red", marginTop:"7px"}}
+                    </i>
+                    
+                    </div>
+
+                    
+                </div>
+            </div>
+    );
+
+}
+
+export default ContactCard;
